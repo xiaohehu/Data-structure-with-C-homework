399c55b6447350d7bff14b1da9cb3a944df4712d
Xiaohe <huxiaohe113@gmail.com>
2016-07-24 11:56:10 -0400

    Problem 1 need to do the calculation

--- Mid_Term/Problem1.cpp
+++ Mid_Term/Problem1.cpp
@@ -10,15 +10,6 @@ bool isValueInArray(int val, int *arr, int size) {
     return false;
 }
 
-int getOperatorIndex(int val, int *arr, int size) {
-	for (int i = 0; i< size; i++) {
-		if (val == arr[i]) {
-			return i;
-		}
-	}
-	return -1;
-}
-
 int main(int argc, char *argv[]) {
 	string inputValue;
 	char numbers[10] = {'1', '2', '3', '4', '5', '6', '7', '8', '9', '0'};
@@ -34,7 +25,6 @@ int main(int argc, char *argv[]) {
 	int operatorIntValue[4];
 	for (int i = 0; i < 4; i++) {
 		operatorIntValue[i] = (int)operators[i];
-		cout << (int)operators[i] << endl;
 	}
 	
 	int numberIntValue[10];
@@ -46,11 +36,11 @@ int main(int argc, char *argv[]) {
 	cin >> inputValue;
 	cout << endl;
 
-// Check input value is valid or not
+// Check input value contains invalid characters
 	bool validValue = true;
 	for (int i = 0; i < inputValue.size(); i++) {
 		int inputIntValue = (int)inputValue[i];
-		validValue = isValueInArray(inputIntValue, validIntValue, sizeof(validIntValue));
+		validValue = isValueInArray(inputIntValue, validIntValue, sizeof(validIntValue)/sizeof(*validIntValue));
 		if (validValue == false) {
 			break;
 		}
@@ -62,26 +52,72 @@ int main(int argc, char *argv[]) {
 
 //Check if the operator is valid or not
 	bool validOperator = true;
-	int operatorIndex = -1;
 	bool nextIsOperator = false;
 	bool devideByZero = false;
+	bool lastIsOperator = false;
 	for (int i = 0; i < inputValue.size(); i++) {
-		cout << i << endl;
 		int inputIntValue = (int)inputValue[i];
-		if (isValueInArray(49, operatorIntValue, sizeof(operatorIntValue))) {
-			cout << inputValue[i] << " " << inputIntValue << endl;
-			nextIsOperator = isValueInArray((int)inputValue[i+1], operatorIntValue, sizeof(operatorIntValue));
+		if (isValueInArray(inputIntValue, operatorIntValue, sizeof(operatorIntValue) / sizeof(*operatorIntValue))) {
+			// Check if the operator is at the end of input string
+			if (i == inputValue.size() - 1) {
+				lastIsOperator = true;
+				break;
+			}
+			// Check if 2 operators are next to each other
+			nextIsOperator = isValueInArray((int)inputValue[i+1], operatorIntValue, sizeof(operatorIntValue) / sizeof(*operatorIntValue));
 			if (nextIsOperator) {
 				break;
 			}
 		}
 	}
-	if (nextIsOperator) {
+	if (nextIsOperator || lastIsOperator) {
 		cout << "Input '" << inputValue << "' formula is not valid! Programe is going to terminated" << endl;
 		return 0;
 	}
-	else {
-		cout << "hahahahha";
+
+// Input value is valid, do the calculation
+	// Get number of operators in formula
+	int operatorNum = 0;
+	for (int i = 0; i < inputValue.size(); i++) {
+		int inputIntValue = (int)inputValue[i];
+		if (isValueInArray(inputIntValue, operatorIntValue, sizeof(operatorIntValue) / sizeof(*operatorIntValue))) {
+			operatorNum++;
+		}
+	}
+	// Create an array to store index of each operator (using dynamic array method)
+	int totalElements = 2 * operatorNum + 1;
+	int *pOperatorIndexArray = NULL;
+	int *pTemp = NULL;
+	int operatorCount = 0;
+	int operatorIndex = 0;
+	for (int i = 0; i < inputValue.size(); i++) {
+		int inputIntValue = (int)inputValue[i];
+		if (isValueInArray(inputIntValue, operatorIntValue, sizeof(operatorIntValue) / sizeof(*operatorIntValue))) {
+			operatorIndex = i;
+			
+			pTemp = new int[operatorCount+1];
+			if (pTemp != NULL){  
+				*(pTemp + operatorCount) = operatorIndex;  
+				for (int j = 0; j < operatorCount; j++)
+					*(pTemp + j) = *(pOperatorIndexArray + j);
+					delete[] pOperatorIndexArray;
+					pOperatorIndexArray = pTemp;
+				}
+			operatorCount++;
+		}
+	}
+	cout << operatorCount << endl;
+	//Create array of all elements in formula
+	int element
+	string elemetsInFormula[2*operatorNum+1];
+	int startIndex = 0;
+	int endIndex =0;
+	for (int i = 0; i < operatorCount; i++) {
+		int currentIndex = *(pOperatorIndexArray+i);
+		if (i == 0) {
+			elemetsInFormula[i] = inputValue.substr(0,currentIndex-1);
+			elemetsInFormula[i+1] = inputValue.substr(currentIndex, 1);
+		}
 	}
 
 }
