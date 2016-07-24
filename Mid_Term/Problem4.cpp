a14760acd484667e1c59453eb75096b8f6daaa9d
Xiaohe <huxiaohe113@gmail.com>
2016-07-24 19:02:05 -0400

    Added problem 4 and finished

---
+++ Mid_Term/Problem4.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+using namespace std;
+int getHeapMemory() {
+	try {
+		unsigned long int i = 1024;
+		unsigned long int sum = 0;
+		int *point;
+		bool leak = false;
+		while (!leak) {
+			point = new int[i];
+			if (point != NULL) {
+				cout << "Address of this part's first byte is: ";
+				cout << point << endl;
+				sum += i;
+				cout << "Currently total loacated heap size is:";
+				cout << sum/256 << "k" << endl;
+				cout << endl;
+				i = i * 2;
+			}
+			else {
+				leak = true;
+				cout << "haha";
+			}
+		}
+	} catch (bad_alloc & ex) {
+		cout << "Memory got leak.";
+		return 0;
+	}
+}
+
+
+int main(int argc, char *argv[]) {
+	return getHeapMemory();
+
+}
