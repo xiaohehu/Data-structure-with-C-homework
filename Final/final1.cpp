fb6e241adbdae71082bce996993617b15769829e
Xiaohe <huxiaohe113@gmail.com>
2016-08-11 18:03:31 -0400

    Added comment to final1 and final2

--- Final/final1.cpp
+++ Final/final1.cpp
@@ -1,3 +1,15 @@
+/*
+READ ME:
+
+	1. create a varible to count times run the function names "runTimes"
+	
+	2. The initial value of the varible is 1 
+	
+	3. Each time when x > 1, the count should increase by 1
+	
+	4. In main(), after get the fibo number, print out the count
+*/
+
 #include <iostream>
 #include <cstdlib>
 using namespace std;
@@ -6,19 +18,22 @@ class FIBO {
 	public:
 		FIBO ();
 		int fibonacci (int);
-		// Create the times count
+		
+		//(1) Create the times count
 		int runTimes;
 	private:
 };
 
 FIBO::FIBO() {
-	// The initial value of the coun is 1
+	
+	//(2) The initial value of the coun is 1
 	runTimes = 1;
 }
 int FIBO::fibonacci (int x) {
 		
-		if ( x > 2 ){
-			//Each time the func is called the count increases by 1
+		if ( x > 1 ){
+			
+			//(3) Each time the func is called the count increases by 1
 			runTimes++;
 	 		return (fibonacci (x - 2) + fibonacci (x - 1));
 		}
@@ -34,7 +49,8 @@ int main ( )
  if (!cin.eof() && cin.good() && inputNumber > 0 && inputNumber <= 20 ) {
  		cout << "Fibonacci number is "; 
 			cout << A.fibonacci (inputNumber) << " ";
-			// Print out the count
+			
+			//(4) Print out the count
 			cout << endl;
 			cout << "Times run the recursion: ";
 			cout << size_t (A.runTimes) << endl;
--- Final/final2.cpp
+++ Final/final2.cpp
@@ -1,10 +1,29 @@
+/*
+READ ME:
+	
+	1. Create a Class names "HashTable" 
+	
+	2. Create 2 functions "makeHashTable" and "printHashTable"
+	
+	3. Make all the varibles as private parameter of the class
+	
+	4. Create Container class
+	
+	5. In the container class create instance of "HashTable class" and also call those 2 functions
+	
+	6. In main(), create an instance of Container to create the hash table.
+*/
+
 #include <iostream>
 #include <string>
 using namespace std;
 #define MAX 23 
 
+// Create the HashTable class and also 2 public function to create & print the table
 class HashTable {
 	private:
+		
+	// All parameters here are using in the 2 functions and they are all private
 		bool full;
 		char inputLine[80];
 		int hashValue, modHash, i;
@@ -90,6 +109,7 @@ void HashTable::printHashTable() {
 	}
 }
 
+// Create the container class and call 2 functions of HashTable class
 class Container {
 	public:
 		void createAndPrintHashTable();
@@ -101,76 +121,8 @@ void Container::createAndPrintHashTable() {
 	hash.printHashTable();
 }
 int main () {
-//	bool full = false;   // boolean set to false, changed to true when hash table full
-//	char inputLine[80] = "";      
-//	int hashValue, modHash, i;
-//	string name [MAX];  // This line and the following two establish the information w
-//	int firstHash [MAX];  // preserved for each entry of the hash array
-//	bool occupied [MAX] = {MAX * false};  //
-//	while (!cin.eof() && full == false) 
-//	{ 
-//		// test for EOF, for hash table not full
-//		cout <<"Please enter a string of characters followed by <Enter>." << endl 
-//			<< "Terminate by entering EOF (control-z on keyboard in Windows." << endl;
-//		cin.getline(inputLine, 80); 
-//		if (!cin.eof()) 
-//		{
-//			hashValue = 0;  // calculate initial hash value
-//			
-//			for (i = 0; i < strlen (inputLine); i++)
-//			{  
-//				hashValue += (int) inputLine[i];      
-//			}
-//			
-//			modHash = hashValue % MAX;	// test initial hash value for available //  array entry
-//			if (occupied[modHash] == false) 
-//			{ 
-//				name[modHash] = inputLine;	
-//				firstHash[modHash] = modHash;
-//				occupied [modHash] = true;
-//				cout << "Your input line " << inputLine << " hashes to "
-//					<< modHash << endl;
-//			}
-//			else   // test failed, look for alternate location in array
-//			{ 
-//				cout << "Hash collision! We need to find an open table entry." << endl;
-//				bool found = false;  // declare binary state variable "found": 
-//						     // false means an available array position 
-//						     // not found, true means it is found.
-//				for (i = 0; i < MAX && found == false && full == false; i++)
-//				{
-//					// test found == false, need to stop execution of this 
-//					// loop when an available array position is found
-//					if (occupied[i] == false)
-//					{ 
-//						// test if selected hash
-//   						// table element is occupied 
-//						name[i] = inputLine;											
-//						firstHash[i] = modHash;
-//						occupied [i] = true;
-//						cout << "Your input line " << inputLine 
-//							<< " hashes to " << i << endl;
-//						found = true; // Set array position found
-//					}
-//					else
-//						if (i >= MAX-1) { 
-//							cout << "Sorry! Table is full!"  <<" We cannot place your entry." 
-//								<< endl << endl;
-//							full = true;  // set state flag to say 
-//							//  hash array to full
-//						}
-//				}
-//			}
-//		}
-//	}
-//		for (i = 0; i < MAX; i++) { // finally, list members of hash table
-//			if (occupied[i] == true) 
-//			{
-//				cout << " Position in table is " << i;
-//				cout << " Hashed to " << firstHash[i];
-//				cout << ".\tString is " << name[i] << endl;
-//			}
-//		}
+	
+// In main(), only create the instance of container class and run function "createAndPrintHashTable()"
 	Container container;
 	container.createAndPrintHashTable();
 	return 0;
